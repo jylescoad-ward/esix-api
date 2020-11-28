9ed2b43fe8fd36334ce271f31be6d2d0a0c95368
Jyles Coad-Ward <jyles@dariox.club>
2020-11-28 18:03:35 +0800

    started working on config-new

--- cli/config-new.js
+++ cli/config-new.js
@@ -4,6 +4,26 @@ module.exports = {
 		description: "create new config, step by step thing like npm init"
 	},
 	f: ()=>{
-		
+		var questions = [
+			{
+				type: 'input',
+				name: 'tags',
+				message: "Enter Tags. Seperate Indivudal Tags with commas, for multiple tags put + inbetween each tag."
+			},
+			{
+				type: 'input',
+				name: 'tagLimit',
+				message: 'Post Limit per Tag (default: 320)'
+			},
+			{
+				type: 'input',
+				name: 'threadLimit',
+				message: 'Maximum ammount of threads/instances for downloading. (default: 4)'
+			}
+		]
+		inquirer.prompt(questions).then((answers) => {
+			console.log(JSON.stringify(answers, null, '  '));
+		});
+		//var previousConfig = gb.cliConfig.all;
 	}
 }
