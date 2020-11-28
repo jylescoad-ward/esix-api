f1d62113e9825974c5b385d366976976eae799f5
Jyles Coad-Ward <jyles@dariox.club>
2020-11-28 19:54:51 +0800

    Update download-overrride.js

--- cli/download-overrride.js
+++ cli/download-overrride.js
@@ -1,9 +1,10 @@
 module.exports = {
 	info: {
 		commands: ["download","--config=<location>"],
+		detection: `download ${process.argv[3] || ""}`,
 		description: "the same as 'esix download' but overrides with the config given."
 	},
-	f: ()=>{
-		
+	f: (argv)=>{
+		require("./download.js").f(argv)
 	}
 }
