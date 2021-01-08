8019f7fd6e48a5ca18d765ec221317f5702b97a4
Jyles Coad-Ward <jylescoadward@gmail.com>
2021-01-08 23:14:44 +0800

    Update post.js - Enabled favorite - Fixed HTTP/HTTPS requests

--- classes/post.js
+++ classes/post.js
@@ -1,45 +1,18 @@
-var req = "";
-const axios = require("axios");
-const pkJSON = require("./../package.json")
-
 var raw = {};
 var creds = {};
 
-module.exports = (g_data,g_creds) => {
+async function req(u,m,d) {
+	const httpRequest = require("./request.js");
+	return httpRequest(u,m,d,creds);
+}
+
+module.exports.gen = (g_data,g_creds) => {
 	creds = g_creds;
 	raw = g_data;
+
 	raw.vote = module.exports.vote;
-	//raw.favorite = module.exports.favorite;
+	raw.favorite = module.exports.favorite;
 	return raw;
-}
-module.exports.req = async (url,g_method,g_data) =>{
-	const options = {
-		method: g_method || 'GET',
-		url: `https://e621.net/${url}`,
-		headers: {
-			'user-agent': `esix-api-${pkJSON.version}`,
-		},
-		data: g_data || {},
-	}
-	options.data.login = creds.username;
-	options.data.api_key = creds.key;
-	options.method = options.method.toUpperCase()
-	if (options.method == "POST" || options.method == "DELETE") {
-		options.headers['content-type'] = "application/json";
-	}
-	const res = await axios(options).catch((e)=>{
-		console.debug(e.toJSON())
-		console.error(`HTTP(S) ERROR: ${e.status} - ${e.statusText}`)
-	})
-	if (options.method == "POST" || options.method == "DELETE") {
-		console.debug(res);
-	}
-	if (res.status != undefined && res.status >= 300) {
-		console.error(`HTTP(S) ERROR: ${req.status} - ${req.statusText}`)
-		return {posts:[]};
-	} else {
-		return res.data;
-	}
 }
 
 module.exports.vote = async (direction,nounvote) => {
@@ -49,7 +22,7 @@ module.exports.vote = async (direction,nounvote) => {
 	} else if (direction == false) {
 		voteDirection = '-1';
 	}
-	var response = await module.exports.req(`posts/${raw.id}/votes.json`,'post',{score:voteDirection,no_unvote:nounvote || true});
+	var response = await req(`posts/${raw.id}/votes.json`,'post',{score:voteDirection,no_unvote:nounvote || true});
 	return response;
 }
 
@@ -59,10 +32,10 @@ module.exports.favorite = async (action) => {
 		return;
 	}
 	if (action) {
-		var response = await module.exports.req(`favorites.json?post_id=${raw.id}`,'post',{post_id:raw.id});
+		var response = await req(`favorites.json?post_id=${raw.id}`,'post',{post_id:raw.id});
 		return response;
 	} else {
-		var response = await module.exports.req(`favorites/${raw.id}.json`,'delete');
+		var response = await req(`favorites/${raw.id}.json`,'delete');
 		return response;
 	}
 }
